9cb70311eb3b6731603855abc366abddd5a1ba19
Wes Lloyd <wlloyd@uw.edu>
2017-08-28 20:45:10 -0700

    Updates, add cpu time accounting to function

--- src/main/java/uwt/Response.java
+++ src/main/java/uwt/Response.java
@@ -12,6 +12,11 @@ package uwt;
 public class Response {
     String value;
     String uuid;
+    String error;
+    long cpuUsr;
+    long cpuKrn;
+    long cutime;
+    long cstime;
     
     public String getValue()
     {
@@ -29,11 +34,68 @@ public class Response {
     {
         this.uuid = uuid;
     }
+    public long getCpuUsr()
+    {
+        return cpuUsr;
+    }
+    public void setCpuUsr(long cpuusr)
+    {
+        this.cpuUsr = cpuusr;
+    }
+    public long getCpuKrn()
+    {
+        return cpuKrn;
+    }
+    public void setCpuKrn(long cpukrn)
+    {
+        this.cpuKrn = cpukrn;
+    }
+    public long getCuTime()
+    {
+        return cutime;
+    }
+    public void setCuTime(long cutime)
+    {
+        this.cutime = cutime;
+    }
+    public long getCsTime()
+    {
+        return cstime;
+    }
+    public void setCsTime(long cstime)
+    {
+        this.cstime = cstime;
+    }
+    public String getError()
+    {
+        return error;
+    }
+    public void setError(String err)
+    {
+        this.error = err;
+    }
     public Response(String value, String uuid)
     {
         this.value = value;
         this.uuid = uuid;
     }
+    public Response(String value, String uuid, long cpuusr, long cpukrn)
+    {
+        this.value = value;
+        this.uuid = uuid;
+        this.cpuUsr = cpuusr;
+        this.cpuKrn = cpukrn;
+    }
+    public Response(String value, String uuid, long cpuusr, long cpukrn, long cutime, long cstime)
+    {
+        this.value = value;
+        this.uuid = uuid;
+        this.cpuUsr = cpuusr;
+        this.cpuKrn = cpukrn;
+        this.cutime = cutime;
+        this.cstime = cstime;
+    }
+    
     public Response()
     {
         
--- src/main/java/uwt/lambda_test.java
+++ src/main/java/uwt/lambda_test.java
@@ -18,6 +18,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 /**
@@ -30,6 +32,7 @@ public class lambda_test implements RequestHandler<Request, Response>
     static Charset CHARSET = Charset.forName("US-ASCII");
     
     public Response handleRequest(Request request, Context context) {
+        CpuTime c1 = getCpuUtilization();
         String uuid = "unset";
         LambdaLogger logger = context.getLogger();
         logger.log("Received=" + request.getName());
@@ -75,7 +78,10 @@ public class lambda_test implements RequestHandler<Request, Response>
                 System.out.println("Sleep was interrupted...");
             }
         }
-        return new Response("Success=" + request.getName(), uuid);
+        CpuTime c2 = getCpuUtilization();
+        CpuTime cused = getCpuTimeDiff(c1, c2);
+        Response r = new Response("Success=" + request.getName(), uuid, cused.utime, cused.stime, cused.cutime, cused.cstime);
+        return r;
     }
     
     
@@ -102,6 +108,85 @@ public class lambda_test implements RequestHandler<Request, Response>
             mult = operand_a[j] * operand_b[j];
             div1 = (double) mult / (double) operand_c[j];
         }
+    }
+    
+    public String getFileAsString(String filename)
+    {
+        File f = new File(filename);
+        Path p = Paths.get(filename);
+        String text = "";
+        StringBuffer sb = new StringBuffer();
+        if (f.exists()) 
+        {
+            try (BufferedReader br = Files.newBufferedReader(p))
+            {
+                while((text = br.readLine()) != null)
+                {
+                    sb.append(text);
+                }
+            }
+            catch (IOException ioe)
+            {
+                sb.append("Error reading file=" + filename);
+            }
+        }
+        return sb.toString();
+    }
+
+    class CpuTime
+    {
+        long utime;
+        long stime;
+        long cutime;
+        long cstime;
+        CpuTime(long utime, long stime, long cutime, long cstime)
+        {
+            this.utime = utime;
+            this.stime = stime;
+            this.cutime = cutime;
+            this.cstime = cstime;
+        }
+        CpuTime()
+        {            
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "utime=" + utime + " stime=" + stime + " cutime=" + cutime + " cstime=" + cstime + " ";
+        }
+    }
+    public CpuTime getCpuUtilization()
+    {
+        String filename = "/proc/1/stat";
+        File f = new File(filename);
+        Path p = Paths.get(filename);
+        String text = "";
+        StringBuffer sb = new StringBuffer();
+        if (f.exists()) 
+        {
+            try (BufferedReader br = Files.newBufferedReader(p))
+            {
+                text = br.readLine();
+                br.close();
+            }
+            catch (IOException ioe)
+            {
+                sb.append("Error reading file=" + filename);
+            }
+            String params[] = text.split(" ");
+            return new CpuTime(Long.parseLong(params[13]),
+                               Long.parseLong(params[14]),
+                               Long.parseLong(params[15]),
+                               Long.parseLong(params[16]));
+        }
+        else
+            return new CpuTime();
+    }
+    
+    public CpuTime getCpuTimeDiff(CpuTime c1, CpuTime c2)
+    {
+        return new CpuTime(c2.utime - c1.utime, c2.stime-c1.stime, c2.cutime - c1.cutime, c2.cstime - c1.cstime);
     }
 
 }
