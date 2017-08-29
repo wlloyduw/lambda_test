d1c8ca76fe247fd09acd67c38f47c85dca3fb092
Wes Lloyd <wlloyd@uw.edu>
2017-08-29 13:09:53 -0700

    added aws lambda and aws gateway cli calls

--- test/callservice.sh
+++ test/callservice.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-json={"\"name\"":\"\/proc\/cpuinfo\"",\"calcs\"":100000,\"sleep\"":10,\"loops\"":10}
-curl -H "Content-Type: application/json" -X POST -d  $json https://ue5e0irnce.execute-api.us-east-1.amazonaws.com/test/test 2>/dev/null 
+json={"\"name\"":\"\/proc\/2\/stat\"",\"calcs\"":100000,\"sleep\"":0,\"loops\"":20}
+
+#call using curl 
+#curl -H "Content-Type: application/json" -X POST -d  $json https://ue5e0irnce.execute-api.us-east-1.amazonaws.com/test/test 2>/dev/null 
+
+#call from aws gateway api
+#aws apigateway test-invoke-method --rest-api-id ue5e0irnce --resource-id dlxjg7 --http-method POST --path-with-query-string '/test' --body $json
+
+#call from aws lambda cli
+aws lambda invoke --invocation-type RequestResponse --function-name test --region us-east-1 --log-type Tail --payload '{"calcs":100000,"sleep":0,"loops":20}' out.txt
+
 
 exit
 
