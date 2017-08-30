edc199bda837d784cee954c44af3d52024a19c95
Wes Lloyd <wlloyd@uw.edu>
2017-08-30 12:08:04 -0700

    updates

--- test/callservice.sh
+++ test/callservice.sh
@@ -16,48 +16,6 @@ curl -H "Content-Type: application/json" -X POST -d  $json https://ue5e0irnce.ex
 exit
 
 
-totalruns=$1
-threads=$2
-
-callservice() {
-  totalruns=$1
-  threadid=$2
-  #host=10.0.0.124
-  #port=8080
-  onesecond=1000
-  if [ $threadid -eq 1 ]
-  then
-    echo "run_id,thread_id,uuid,elapsed_time,sleep_time_ms"
-  fi
-  for (( i=1 ; i <= $totalruns; i++ ))
-  do
-    json={"\"name\"":\"Fred\"",\"calcs\"":100,\"sleep\"":10,\"loops\"":10}
-    time1=( $(($(date +%s%N)/1000000)) )
-    #uuid=`curl -H "Content-Type: application/json" -X POST -d "{\"name\": \"Fred\"}" https://ue5e0irnce.execute-api.us-east-1.amazonaws.com/test/test 2>/dev/null | cut -d':' -f 3 | cut -d'"' -f 2` 
-    uuid=`curl -H "Content-Type: application/json" -X POST -d  $json https://ue5e0irnce.execute-api.us-east-1.amazonaws.com/test/test 2>/dev/null | cut -d':' -f 3 | cut -d'"' -f 2` 
-    time2=( $(($(date +%s%N)/1000000)) )
-    elapsedtime=`expr $time2 - $time1`
-    sleeptime=`echo $onesecond - $elapsedtime | bc -l`
-    sleeptimems=`echo $sleeptime/$onesecond | bc -l`
-    echo "$i,$threadid,$uuid,$elapsedtime,$sleeptimems"
-    if (( $sleeptime > 0 ))
-    then
-      sleep $sleeptimems
-    fi
-  done
-}
-export -f callservice
-
-runsperthread=`echo $totalruns/$threads | bc -l`
-runsperthread=${runsperthread%.*}
-date
-echo "Setting up test: runsperthread=$runsperthread threads=$threads totalruns=$totalruns"
-for (( i=1 ; i <= $threads ; i ++))
-do
-  arpt+=($runsperthread)
-done
-parallel --no-notice -j $threads -k callservice {1} {#} ::: "${arpt[@]}"
-
 
 
 
