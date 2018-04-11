0191631cfbf888325d897a1256b4953711dc688c
Wes Lloyd <wlloyd@uw.edu>
2018-04-10 22:09:29 -0700

    updates

---
+++ test/cs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+json={"\"name\"":"\"whoami\",\"calcs\"":200000,\"sleep\"":0,\"loops\"":20}
+echo $json
+
+#call using curl 
+curl -H "Content-Type: application/json" -X POST -d  $json https://ue5e0irnce.execute-api.us-east-1.amazonaws.com/test/test 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#call from aws gateway api
+#aws apigateway test-invoke-method --rest-api-id ue5e0irnce --resource-id dlxjg7 --http-method POST --path-with-query-string '/test' --body $json
+
+#call from aws lambda cli
+#aws lambda invoke --invocation-type RequestResponse --function-name test --region us-east-1 --log-type Tail --payload '{"calcs":100000,"sleep":0,"loops":20}' out.txt
+#aws lambda invoke --invocation-type RequestResponse --function-name test --region us-east-1 --log-type Tail --payload $json out.txt
+
+
+exit
+
+
+
+
+
+
